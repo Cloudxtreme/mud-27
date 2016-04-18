6c8c475e6f36f514cdad97ec7061e0ffe775dce2
Norskan <simon.angerer@hotmail.com>
2016-04-18 21:39:45 +0200

    added test for character

---
+++ test/character_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Norskan/mud/gameworld"
+)
+
+func TestCharacterNewCharacter(t *testing.T) {
+	newCharacter := gameworld.NewCharacter()
+
+	if !(newCharacter != nil) {
+		t.Error("Expected a new Character")
+	}
+}
+
+func TestGetTilePosition(t *testing.T) {
+	newCharacter := gameworld.NewCharacter()
+	gameTile := gameworld.NewDefaultTile(actualID, actualMark)
+
+	newCharacter.SetTilePosition(gameTile)
+
+	if !(newCharacter.GetTilePositon() == gameTile) {
+		t.Error("Expected a tile on tilePosition")
+	}
+}
